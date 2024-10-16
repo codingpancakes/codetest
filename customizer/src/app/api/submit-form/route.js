04d04692a6e6b748a42c0ff234a5f3547cb502ca
codingpancakes <jon@than.red>
2024-10-16 17:11:04 -0500

    fix server submit and style

--- customizer/src/app/api/submit-form/route.js
+++ customizer/src/app/api/submit-form/route.js
@@ -13,7 +13,6 @@ export async function POST(request) {
       const formData = await request.formData();
       const yardSpace = formData.get('yardSpace');
       const style = formData.getAll('style');
-      const _gardenPhoto = formData.get('gardenPhoto'); 
       
       await new Promise((resolve) => setTimeout(resolve, 3000));
   
